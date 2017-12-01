50be705e905351ffd6b549474becf05fe744aeab
nsandeep <nsandeepdotcom@gmail.com>
2017-12-01 00:43:10 -0600

    nsandeep-scheduler-service intial commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+walkthroughs
+
+1.Scheduling a Task using Cron Expression
+2.Scheduling a Task with Fixed Rate
+3.Scheduling a Task With Fixed Rate and Initial Delay
+4.Scheduling a Task with Fixed Delay
---
+++ src/main/java/com/nsandeep/scheduler/NsandeepSchedulerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nsandeep.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsandeepSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsandeepSchedulerServiceApplication.class, args);
+	}
+}
